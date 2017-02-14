6838b8c78b3cb7ef672645ea5d8dd26c67b3def6
keshannw <keshannw@gmail.com>
2017-02-14 23:30:08 +0530

    Initial Commit

---
+++ GitCommitApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitCommitApp
+//
+//  Created by Keshan N Withanage on 2/14/17.
+//  Copyright © 2017 KeshWithZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
